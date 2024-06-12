9e1755536ee5997ee7414bb82ad9f7f40ff35351
mazenhesham17 <mazenhesham7@live.com>
2024-06-13 00:52:36 +0300

    try to fix paragraph starting indentation

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -19,8 +19,8 @@ export const Card = (props: CardProps) => {
     <li id={`card-${props.id}`} className="relative p-4 bg-gray-50 border border-gray-200 rounded-lg max-w-md">
       {props.imageUrl && (
         <>
-          <div className="absolute top-2 left-2 w-12 h-12">
-            <img src='https://firebasestorage.googleapis.com/v0/b/seniors-24.appspot.com/o/assets%2Fgown.png?alt=media&token=8ecce6e6-aa5e-4f09-8b71-cd1d3132702a' alt="gown" className="w-full h-full object-cover" />
+          <div className="absolute top-2 left-0 w-14 h-14">
+            <img src='https://firebasestorage.googleapis.com/v0/b/seniors-24.appspot.com/o/assets%2Fgown.png?alt=media&token=8ecce6e6-aa5e-4f09-8b71-cd1d3132702a' alt="gown" className="w-full h-auto" />
           </div>
           <div className="mt-2">
             <img src={props.imageUrl} alt="Message" className="w-full h-full object-cover rounded" />
@@ -28,14 +28,15 @@ export const Card = (props: CardProps) => {
         </>
 
       )}
-      <div id={`scroll-element-${props.id}`} className={`overflow-y-auto max-w-full ${props.imageUrl ? 'max-h-40 mt-4' : 'max-h-120'}`}>
-        <p className={`text-base break-words ${isArabic ? 'text-right' : 'text-left'}`}>{props.message}</p>
-      </div>
-      <p className="text-base font-bold text-left">{props.sender}</p>
+      <p id={`scroll-element-${props.id}`} className={`overflow-y-auto max-w-full ${props.imageUrl ? 'max-h-40 mt-4' : 'max-h-120'} text-base break-words ${isArabic ? 'text-right' : 'text-left'}`}>
+        {props.message}
+      </p>
+      <p className="text-base font-bold text-left">
+        {props.sender}
+      </p>
       <div className={`flex items-center justify-${downloading ? 'between' : 'end'}`} >
-        {downloading && <p className="text-sm text-gray-500">Preparing for download...</p>}
+        {downloading && <p className="sm:text-xs  md:text-sm text-gray-500">Preparing for download...</p>}
         <div className="flex items-center justify-end space-x-2">
-
           <FontAwesomeIcon id={`hide-element-${props.id}`} className="text-primary cursor-pointer"
             icon={faDownload}
             size='lg'
@@ -45,7 +46,7 @@ export const Card = (props: CardProps) => {
               setDownloading(false)
             }} />
           <p className="text-sm text-gray-500">{props.date}</p>
-          <img src='https://firebasestorage.googleapis.com/v0/b/seniors-24.appspot.com/o/assets%2Fsignature.png?alt=media&token=a98222d8-7170-4ada-9e12-9839b14256e5' alt="signature" className="h-10 object-cover" />
+          <img src='https://firebasestorage.googleapis.com/v0/b/seniors-24.appspot.com/o/assets%2Fsignature.png?alt=media&token=a98222d8-7170-4ada-9e12-9839b14256e5' alt="signature" className="h-10" />
         </div>
       </div>
 
